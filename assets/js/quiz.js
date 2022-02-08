1106b4c077a8523a6c0fb8308518cd42a1e156b5
Radee1 <roseadee2013@gmail.com>
2022-02-08 15:38:38 +0000

    Add function to calculate scores, add function to show and hide the quiz questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,8 +1,10 @@
 var score = 0;
 
 /**
- * function for the creating the username
+ * custom function called username() which checks user input on the page
+ * and lets the users access the quiz after entering the required username length
  */
+ //function for username
 function username() {
     var input = document.getElementById("name").value;
     if (input == "") {
@@ -10,7 +12,28 @@ function username() {
     } else if (input.length < 3) {
         alert("Name must be atleast 3 characters");
     } else {
-        alert("Hi '+input+' and welcome to Uganda online quiz gane! Press Ok to continue to the quiz");
+        alert("Hi '+input+' and welcome to Uganda online quiz game! Press Ok to continue to the quiz");
         window.location = "page1.html";
     }
+}
+
+//function for counting up the score
+function calc(){
+    score++;
+}
+
+/** 
+* On page1.html load, functions show() and hide() reveal and remove the quiz questions by element ID.
+* They are used to loop through the quiz questions by hiding all questions and only showing the one to be answered at a time. 
+*/
+//hiding elements by id
+function hide(x){
+    var target = document.getElementById(x);
+    target.setAttribute("style", "display:none;");
+}
+
+//showing elements by id
+function show(y){
+    var target = document.getElementById(y);
+    target.setAttribute("style", "display:inline;");
 }
