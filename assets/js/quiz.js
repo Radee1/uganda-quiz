cef4fbcc9af6bee73855494bfcec29055675f4fe
Radee1 <roseadee2013@gmail.com>
2022-02-09 00:19:23 +0000

    Add description for the ug quiz site and mockup image to readme

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,7 +4,7 @@ var score = 0;
  * custom function called username() which checks user input on the page
  * and lets the users access the quiz after entering the required username length
  */
- //function for username
+//function for username
 function username() {
     var input = document.getElementById("name").value;
     if (input == "") {
@@ -12,28 +12,28 @@ function username() {
     } else if (input.length < 3) {
         alert("Name must be atleast 3 characters");
     } else {
-        alert("Hi '+input+' and welcome to Uganda online quiz game! Press Ok to continue to the quiz");
+        alert(`Hi ${input} and welcome to Uganda online quiz game! Press Ok to continue to the quiz`);
         window.location = "page1.html";
     }
 }
 
 //function for counting up the score
-function calc(){
+function calc() {
     score++;
 }
 
 /** 
-* On page1.html load, functions show() and hide() reveal and remove the quiz questions by element ID.
-* They are used to loop through the quiz questions by hiding all questions and only showing the one to be answered at a time. 
-*/
+ * On page1.html load, functions show() and hide() reveal and remove the quiz questions by element ID.
+ * They are used to loop through the quiz questions by hiding all questions and only showing the one to be answered at a time. 
+ */
 //hiding elements by id
-function hide(x){
+function hide(x) {
     var target = document.getElementById(x);
     target.setAttribute("style", "display:none;");
 }
 
 //showing elements by id
-function show(y){
+function show(y) {
     var target = document.getElementById(y);
     target.setAttribute("style", "display:inline;");
 }
@@ -173,7 +173,7 @@ function saf() {
 }
 
 //questionThree about independence
-function correct3(){
+function correct3() {
     calc();
     var input = document.getElementById("uhuru");
     input.setAttribute("class", "choice");
